b06430122e4cc44949016f2601db87e2ad730f60
Jack Gallacher <jackleslie.gallacher@gmail.com>
2024-11-18 21:12:47 +0000

    checkout and special price code to pass the tests.

--- Checkout/Checkout.cs
+++ Checkout/Checkout.cs
@@ -2,13 +2,42 @@
 
 public class Checkout : ICheckout
 {
+    private readonly Dictionary<string, int> prices;
+    private readonly Dictionary<string, SpecialPrice> specialOffers;
+    private readonly Dictionary<string, int> scannedItems;
+
+    public Checkout(Dictionary<string, int> prices, Dictionary<string, SpecialPrice> specialOffers)
+    {
+        this.prices = prices;
+        this.specialOffers = specialOffers;
+        scannedItems = new Dictionary<string, int>();
+    }
+    
     public void Scan(string item)
     {
-        throw new NotImplementedException();
+        scannedItems.TryAdd(item, 0);
+        scannedItems[item]++;
     }
 
     public int GetTotalPrice()
     {
-        throw new NotImplementedException();
+        var totalPrice = 0;
+
+        foreach (var item in scannedItems)
+        {
+            if (specialOffers.TryGetValue(item.Key, out var offer))
+            {
+                var offerCount = item.Value / offer.Quantity;
+                
+                totalPrice += offerCount * offer.Price;
+                totalPrice += (item.Value % offer.Quantity) * prices[item.Key];
+            }
+            else
+            {
+                totalPrice += item.Value * prices[item.Key];
+            }
+        }
+
+        return totalPrice;
     }
 }
--- Checkout/SpecialPrice.cs
+++ Checkout/SpecialPrice.cs
@@ -2,5 +2,12 @@
 
 public class SpecialPrice
 {
-    
+    public int Quantity { get; set; }
+    public int Price { get; set; }
+
+    public SpecialPrice(int quantity, int price)
+    {
+        Quantity = quantity;
+        Price = price;
+    }
 }
